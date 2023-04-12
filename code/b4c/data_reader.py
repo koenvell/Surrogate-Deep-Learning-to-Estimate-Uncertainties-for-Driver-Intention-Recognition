c53043f0bf3828e4929dbbe15c4aecacfe94c036
Koen Vellenga <129508993+koenvell@users.noreply.github.com>
2023-04-12 08:31:22 +0200

    Create data_reader.py

---
+++ code/b4c/data_reader.py
@@ -0,0 +1,94 @@
+import cPickle
+import numpy # otherwise opening the pickle file fails
+import numpy as np
+import pandas as pd
+from glob import glob
+
+
+def b4c_parse_train_data(file_path):
+    """ Requires python 2.7!!! 
+    Download train data from: 
+    https://github.com/asheshjain399/RNNexp/tree/7fc5a53292dc0f232867beb66c3a9ef845d705cb/anticipatory-rnn/maneuver-anticipation/checkpoints/all_new_features/
+
+    Args:
+        file_path(str)
+
+    Returns:
+        pd.DataFrame
+    """
+    # init empty dataframe
+    col_names = ["id", "t", "label",
+                 "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12",  # face-tracking features
+                 "x13", "x14", "x15", "x16", ]  # road features
+    df = pd.DataFrame(columns=col_names)
+
+    train_data = cPickle.load(open(file_path))
+    print(train_data['labels'])
+    Y_tr = train_data['labels']
+    X_tr = train_data['features']
+
+    for i in range(len(X_tr)):
+        train_samples = X_tr[:, i:i+1, :]
+
+        for j,k in enumerate(train_samples):
+            # add an index number, timestep and label
+            label = Y_tr[j][i]
+            id_t = np.array([int(i), int(j), int(label)]) # id, time, label
+            row = np.concatenate((id_t, k[0]))
+            # add as row to df
+            df = df.append(pd.DataFrame(row.reshape(1,-1), columns=col_names), ignore_index=True)
+    return df
+
+def b4c_parse_test_data(file_path):
+    """ Requires python 2.7!!! 
+    Download test data from: 
+    https://github.com/asheshjain399/RNNexp/tree/7fc5a53292dc0f232867beb66c3a9ef845d705cb/anticipatory-rnn/maneuver-anticipation/checkpoints/all_new_features/
+    
+    Args:
+        file_path(str)
+        
+    Returns: 
+        pd.DataFrame 
+    """
+    # initialize dataframe with columns
+    col_names = ["id", "t", "label", 
+                 "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", #  face-tracking features
+                 "x13", "x14", "x15", "x16", ]  # road features
+    df = pd.DataFrame(columns=col_names)
+
+    # open pickle file
+    test_data = cPickle.load(open(file_path))
+    Y_te = test_data['labels']
+    X_te = test_data['features']
+
+    for i in range(len(X_te)):
+        test_samples = X_te[i]
+
+        for j, k in enumerate(test_samples):
+            label = Y_te[i][j]  # identifier - timestep
+            id_t = np.array([int(i), int(j), int(label)])  # id, t, label
+            row = np.concatenate((id_t, k[0]))
+            # add as row to df
+            df = df.append(pd.DataFrame(row.reshape(1, -1), columns=col_names), ignore_index=True)
+
+    return df
+
+
+def main():
+    file_path = './original_data/train_data_*.pik'
+    filepath_list = glob(file_path)
+    for i, filepath in enumerate(filepath_list):
+        # Example run of the python 2.7 pickle parse script
+        df = b4c_parse_train_data(filepath)
+        df.to_pickle('b4c_train_data_{}.p'.format(str(i)))
+    
+    # lazy coding
+    file_path = './original_data/test_data_*.pik'
+    filepath_list = glob(file_path)
+    for i, filepath in enumerate(filepath_list):
+        # Example run of the python 2.7 pickle parse script
+        df = b4c_parse_test_data(filepath)
+        df.to_pickle('b4c_test_data_{}.p'.format(str(i)))
+
+if __name__ == "__main__":
+    main()
