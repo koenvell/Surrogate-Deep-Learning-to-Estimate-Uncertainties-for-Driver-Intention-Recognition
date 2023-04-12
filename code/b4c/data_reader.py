2e549d62b8261e9f7909d96a6d23859f4268d040
Koen Vellenga <129508993+koenvell@users.noreply.github.com>
2023-04-12 08:37:36 +0200

    Update data_reader.py

--- code/b4c/data_reader.py
+++ code/b4c/data_reader.py
@@ -1,66 +1,66 @@
-import cPickle
+import cPickle  # for working with pickled data
 import numpy # otherwise opening the pickle file fails
-import numpy as np
-import pandas as pd
-from glob import glob
+import numpy as np  # for working with arrays
+import pandas as pd  # for working with dataframes
+from glob import glob  # for searching files by pattern
 
 
 def b4c_parse_train_data(file_path):
-    """ Requires python 2.7!!! 
-    Download train data from: 
-    https://github.com/asheshjain399/RNNexp/tree/7fc5a53292dc0f232867beb66c3a9ef845d705cb/anticipatory-rnn/maneuver-anticipation/checkpoints/all_new_features/
+    """Parses the training data pickle file and returns a Pandas dataframe.
 
     Args:
-        file_path(str)
+        file_path (str): Path to the pickle file.
 
     Returns:
-        pd.DataFrame
+        pd.DataFrame: A dataframe containing the parsed data.
     """
-    # init empty dataframe
+    # Initialize empty dataframe with column names
     col_names = ["id", "t", "label",
                  "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12",  # face-tracking features
                  "x13", "x14", "x15", "x16", ]  # road features
     df = pd.DataFrame(columns=col_names)
 
+    # Load data from pickle file
     train_data = cPickle.load(open(file_path))
-    print(train_data['labels'])
     Y_tr = train_data['labels']
     X_tr = train_data['features']
 
+    # Iterate over data and add to dataframe
     for i in range(len(X_tr)):
         train_samples = X_tr[:, i:i+1, :]
 
-        for j,k in enumerate(train_samples):
-            # add an index number, timestep and label
+        for j, k in enumerate(train_samples):
+            # add an index number, timestep, and label
             label = Y_tr[j][i]
             id_t = np.array([int(i), int(j), int(label)]) # id, time, label
             row = np.concatenate((id_t, k[0]))
-            # add as row to df
+            # add as row to dataframe
             df = df.append(pd.DataFrame(row.reshape(1,-1), columns=col_names), ignore_index=True)
+
     return df
 
+
 def b4c_parse_test_data(file_path):
-    """ Requires python 2.7!!! 
-    Download test data from: 
-    https://github.com/asheshjain399/RNNexp/tree/7fc5a53292dc0f232867beb66c3a9ef845d705cb/anticipatory-rnn/maneuver-anticipation/checkpoints/all_new_features/
-    
+    """Parses the test data pickle file and returns a Pandas dataframe.
+
     Args:
-        file_path(str)
-        
-    Returns: 
-        pd.DataFrame 
+        file_path (str): Path to the pickle file.
+
+    Returns:
+        pd.DataFrame: A dataframe containing the parsed data.
     """
-    # initialize dataframe with columns
-    col_names = ["id", "t", "label", 
-                 "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", #  face-tracking features
+    # Initialize empty dataframe with column names
+    col_names = ["id", "t", "label",
+                 "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", # face-tracking features
                  "x13", "x14", "x15", "x16", ]  # road features
     df = pd.DataFrame(columns=col_names)
 
-    # open pickle file
+    # Load data from pickle file
     test_data = cPickle.load(open(file_path))
     Y_te = test_data['labels']
     X_te = test_data['features']
 
+    # Iterate over data and add to dataframe
     for i in range(len(X_te)):
         test_samples = X_te[i]
 
@@ -68,7 +68,7 @@ def b4c_parse_test_data(file_path):
             label = Y_te[i][j]  # identifier - timestep
             id_t = np.array([int(i), int(j), int(label)])  # id, t, label
             row = np.concatenate((id_t, k[0]))
-            # add as row to df
+            # add as row to dataframe
             df = df.append(pd.DataFrame(row.reshape(1, -1), columns=col_names), ignore_index=True)
 
     return df
